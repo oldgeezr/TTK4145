b2a04d18dd8621c625987a72bcb78e92771b7a79
Frede Fardal <frede.fardal@gmail.com>
2014-01-30 11:26:41 +0100

    Updated network.go

--- network.go
+++ network.go
@@ -4,35 +4,39 @@ import(
         ."fmt" 
         "time"
         ."net"
+        "strings"
 )
 
 func send_message(conn Conn) {
-
-        str := "Hallo!"
+        
+        str := GetMyIP() + "Hei!"
         _, err := conn.Write([]byte(str))
         _ = err
 }
 
 func udp_listen(ch chan bool) {
 
-        saddr, _ := ResolveUDPAddr("udp", ":20010")        
+        saddr, _ := ResolveUDPAddr("udp", ":10020")        
         ln, _ := ListenUDP("udp", saddr)
         
         for {
                 b2 := make([]byte,1024)
                 _, _, err := ln.ReadFromUDP(b2) 
-                _ = err   
-                Println(string(b2))
+                _ = err
                 if err == nil {
                         time.Sleep(time.Second)
                         ch<- true
+                }
+                
+                if string(b2[0:15]) != GetMyIP() {
+                    Println("UDP from " + string(b2[0:15]) + " saying " + string(b2[15:200]))
                 }
         }
 }
 
 func udp_send(ch chan bool) {
 
-        saddr, _ := ResolveUDPAddr("udp","129.241.187.255:20009")
+        saddr, _ := ResolveUDPAddr("udp","129.241.187.255:10020")
         conn, _ := DialUDP("udp", nil, saddr)
         
         for {
@@ -43,6 +47,28 @@ func udp_send(ch chan bool) {
                     send_message(conn)
                 }
         }        
+}
+
+func GetMyIP() string {
+        allIPs, err := InterfaceAddrs()
+        if err != nil {
+                Println("network.GetMyIP()--> Error receiving IPs")
+                return ""
+        }
+        
+        IPString := make([]string, len(allIPs))
+        for i := range allIPs {
+                temp := allIPs[i].String()
+                ip := strings.Split(temp, "/")
+                IPString[i] = ip[0]
+        }
+        var myIP string
+        for i:=range IPString {
+                if IPString[i][0:3] == "129" {
+                        myIP = IPString[i]
+                }
+        }
+        return myIP
 }
 
 func main() {
