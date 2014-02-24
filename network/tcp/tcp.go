34ee2aeab2a58b3fd0070f98fd2757f705f10a0b
oldgeezr <ramstadevensen@hotmail.com>
2014-02-24 14:54:58 +0100

    jsjs

--- network/tcp/tcp.go
+++ network/tcp/tcp.go
@@ -9,59 +9,26 @@ import (
 	"time"
 )
 
-func TCP_Send(conn Conn, msg string) {
+func TCP_listen() {
+	println("Starting the server")
 
-	time.Sleep(1100 * time.Millisecond)
-	_, err := conn.Write([]byte(msg))
-	_ = err
-}
-
-func TCP_read(conn Conn) {
-
-	b := make([]byte, 1024)
-	for {
-		conn.Read(b)
-	}
-}
-
-func TCP_echo() {
-
-	// Listen for incoming connections.
-	l, err := Listen("tcp", TCP_PORT)
+	listener, err := net.Listen("tcp", "0.0.0.0:6666")
 	if err != nil {
-		Println("Error listening:", err.Error())
+		println("error listening:", err.Error())
 		os.Exit(1)
 	}
-	// Close the listener when the application closes.
-	defer l.Close()
+
 	for {
-		// Listen for an incoming connection.
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			Println("Error accepting: ", err.Error())
-			os.Exit(1)
+			println("Error accept:", err.Error())
+			return
 		}
-		// Handle connections in a new goroutine.
-		go handleRequest(conn)
+		go EchoFunc(conn)
 	}
-
 }
 
-func handleRequest(conn Conn) {
-	// Make a buffer to hold incoming data.
-	buf := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
-	_, err := conn.Read(buf)
-	if err != nil {
-		Println("Error reading:", err.Error())
-	}
-	// Send a response back to person contacting us.
-	conn.Write([]byte("Message received."))
-	// Close the connection when you're done with it.
-	conn.Close()
-}
-
-func EchoFunc(conn Conn) {
+func EchoFunc(conn net.Conn) {
 	buf := make([]byte, RECV_BUF_LEN)
 	n, err := conn.Read(buf)
 	if err != nil {
@@ -71,25 +38,13 @@ func EchoFunc(conn Conn) {
 	println("received ", n, " bytes of data =", string(buf))
 
 	//send reply
-	_, err = conn.Write(buf)
-	if err != nil {
-		println("Error send reply:", err.Error())
-	} else {
-		println("Reply sent")
-	}
-}
-
-func MASTER_TCP_read() {
-
-	ln, _ := Listen("tcp", TCP_PORT)
-
 	for {
-
-		time.Sleep(500 * time.Millisecond)
-		conn, _ := ln.Accept()
-		_ = conn
-
-		go TCP_echo()
+		_, err = conn.Write(buf)
+		if err != nil {
+			println("Error send reply:", err.Error())
+		} else {
+			println("Reply sent")
+		}
 	}
 }
 
