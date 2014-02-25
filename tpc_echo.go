3947d43a38aa65ddc3ae95ca72e6053fcf157533
oldgeezr <ramstadevensen@hotmail.com>
2014-02-25 12:26:36 +0100

    saddas

---
+++ tpc_echo.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	. "fmt"
+	. "net"
+	"time"
+)
+
+func main() {
+
+	go TCP_listen()
+	go TCP_send()
+
+	neverQuit := make(chan string)
+	<-neverQuit
+
+}
+
+func TCP_listen() {
+
+	ln, err := Listen("tcp", ":27731")
+	Println(err, "listen")
+
+	for {
+
+		conn, _ := ln.Accept()
+		go TCP_echo(conn)
+	}
+
+}
+
+func TCP_echo(conn Conn) {
+
+	b := make([]byte, 1024)
+	conn.Read(b)
+	Println(string(b))
+}
+
+func TCP_send() {
+
+	Println("I was here")
+
+	conn, err := Dial("tcp", "129.241.187.255:27731")
+	Println(err, "send")
+	for {
+		time.Sleep(time.Second)
+		b := make([]byte, 1024)
+		b = []byte("yei!")
+		conn.Write(b)
+	}
+}
