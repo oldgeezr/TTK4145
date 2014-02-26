2a2e91da9de25bcd5d6c6fb9861f30359aa41c17
oldgeezr <ramstadevensen@hotmail.com>
2014-02-26 19:05:23 +0100

    Log

--- lift/log/log.go
+++ lift/log/log.go
@@ -1,74 +1,100 @@
 package log
 
-// NOTE TO SELF!
-// This works!
-/*
-
-	ext_queue := []slice{}
-	job_queue := []jobs{}
-
-	ext_queue = append(ext_queue, slice{2}, slice{3})
-	job_queue = append(job_queue, jobs{ext_queue})
-
-	Println(ext_queue)
-
-*/
-
-import (
-	. "fmt"
-)
-
-const (
+/*const (
 	N int = 3 // Antall heiser
-)
+)*/
 
-type dist struct {
-	ip    string
-	floor int
+type Dict struct {
+	Ip    string
+	Floor int
 }
 
-type slice struct {
-	a int
+type Slice struct {
+	A int
 }
 
-type jobs struct {
-	ip string
-	b  []slice
+type Jobs struct {
+	Ip   string
+	Dest []Slice
 }
 
-func Test_queue(last_floor, int_order chan dist, ext_order chan dist) {
+type Order struct {
+	Ip    string
+	Pos   int
+	Floor int
+}
 
-	last_queue := []dist{}
-	int_queue := []dist{}
-	ext_queue := []slice{}
-	job_queue := []jobs{}
+func Pop_first(this []Slice) []Slice {
 
-	last_queue = append(last_queue, dist{"147", 2}, dist{"151", 1}, dist{"146", 3})
+	return this[1:len(this)]
+}
 
-	for _, lifts := range last_queue {
+func Insert(this []Slice, pos Order.Pos, floor Order.Floor) []Slice {
 
-		job_queue = append(job_queue, jobs{lifts.ip, []slice{}})
-	}
+	// HÅper dette funker
+	temp_slice := []Slice{}
+	temp_slice = this[:pos-1]
+	temp_slice = append(temp_slice, floor)
+	temp_slice = append(temp_slice, this[pos:])
+	return temp_slice
+}
+
+func Last_queue(last_floor, get_last_queue chan Dict, new_job_queue chan string, algo_out chan Order) {
+
+	last_queue := []Dict{}
 
 	for {
 		select {
 		case msg := <-last_floor:
 			missing := false
 			for _, last := range last_queue {
-				if msg.ip == last.ip {
-					last.floor = msg.floor
+				if msg.Ip == last.Ip {
+					last.Floor = msg.Floor
 				} else {
 					missing = true
 				}
 			}
 			if missing {
-				last_queue = append(last_queue, dist{msg})
+				last_queue = append(last_queue, Dict{msg})
+				new_job_queue <- msg.Ip
 			}
-		case msg := <-int_order:
-			int_queue = append(int_queue, dist{msg})
-		case msg := <-ext_order:
-			ext_queue = append(ext_queue, dist{msg})
+		case get_last_queue <- last_queue:
+			// Må kanskje ha ein default med time sleep
 		}
 	}
+}
 
+func Job_queues(new_job_queue, master_request, master_pop chan string, algo_out chan Order) {
+
+	job_queue := []Jobs{}
+
+	for {
+		select {
+		case ip := <-new_job_queue:
+			// Opprett ny kø på gitt ip
+			job_queue = append(job_queue, Jobs{ip, []Slice{}})
+		case Do := <-algo_out:
+			// Legg til beslutning fra algo i rett jobb kø
+			for _, queue := range job_queue {
+				if queue.Ip == ip {
+					queue = Insert(queue, Do.Pos, Do.Floor)
+					master_request <- queue.Dest[0]
+				}
+			}
+		case ip := <-master_request:
+			// Send ny ordre fra riktig kø til master
+			for _, queue := range job_queue {
+				if queue.Ip == ip {
+					master_request <- queue.Dest[0]
+				}
+			}
+		case ip := <-master_pop:
+			// pop ordre fra kø, da den er fullførrt
+			for _, queue := range job_queue {
+				if queue.Ip == ip {
+					queue = Pop_first(queue)
+				}
+			}
+		}
+	}
 }
--- test.go
+++ test.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	. "./lift/log"
+	. "fmt"
 )
 
 func main() {
 
-	Test_queue()
+	last_queue := []Dict{}
+	last_queue = append(last_queue, Dict{"147", 2}, Dict{"151", 1}, Dict{"146", 3})
+	Println(last_queue)
+
+	pop, last_queue := Pop_first(last_queue)
+
+	Println(pop, ":", last_queue)
+
 }
