ec9e904143b9c47f459b37e5bf395813c6986757
oldgeezr <ramstadevensen@hotmail.com>
2014-02-27 19:59:36 +0100

    commit

--- network/tcp/tcp.go
+++ network/tcp/tcp.go
@@ -97,7 +97,6 @@ func TCP_slave_send(master_ip string, int_order, ext_order, last_order chan Dict
 	}()*/
 
 	for {
-		b := make([]byte, BUF_LEN)
 		select {
 		case msg := <-int_order:
 			b, err := json.Marshal(msg)
