8c7dd45a5edd4f1c9b251809c08bc7b5ba2e0585
oldgeezr <ramstadevensen@hotmail.com>
2014-03-05 15:12:52 +0100

    testing

--- network/tcp/tcp.go
+++ network/tcp/tcp.go
@@ -71,11 +71,11 @@ func TCP_slave_com(master_ip string, order chan Dict, queues chan Queues) {
 				Println("close connection")
 				return
 
-			} /*else {
-				var c Queues
+			} else {
+				/*var c Queues
 				json.Unmarshal(b[0:length], &c)
-				queues <- c
-			}*/
+				queues <- c*/
+			}
 		}
 	}
 }
