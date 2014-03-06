cca05f97602bdef42aad0eec9a086e2705ae299b
oldgeezr <ramstadevensen@hotmail.com>
2014-03-06 12:43:25 +0100

    testing

--- lift/log/log.go
+++ lift/log/log.go
@@ -11,15 +11,34 @@ func Job_queues(master_order, slave_order, get_at_floor chan Dict, queues, get_q
 	job_queue := []Jobs{}
 	ext_queue := []Dict{}
 	the_queue := Queues{job_queue, ext_queue}
-	order := make(chan Dict)
 
 	for {
 		select {
 		case msg := <-master_order:
-			go func() {order<- msg}()
+			if msg.Dir == "int" {
+				job_queue, _ = AIM_Jobs(job_queue, msg.Ip_order)
+				for i, job := range job_queue {
+					if job.Ip == msg.Ip_order {
+						job_queue[i].Dest, _ = AIM_Int(job_queue[i].Dest, msg.Floor)
+					}
+				}
+				the_queue = Queues{job_queue, ext_queue}
+				queues <- the_queue
+				do_first <- the_queue
+			} else if msg.Ip_order == "ext" {
+				ext_queue, _ = AIM_Spice(ext_queue, msg.Floor, msg.Dir)
+				the_queue = Queues{job_queue, ext_queue}
+				queues <- the_queue
+				do_first <- the_queue
+			} else if msg.Dir == "last" {
+				var update bool 
+				last_queue, update = AIM_Dict(last_queue, msg)
+				if update {
+					get_at_floor <- msg
+					Println(last_queue)
+				}
+			}
 		case msg := <- slave_order:
-			go func() {order<- msg}()
-		case msg := <- order:
 			if msg.Dir == "int" {
 				job_queue, _ = AIM_Jobs(job_queue, msg.Ip_order)
 				for i, job := range job_queue {
