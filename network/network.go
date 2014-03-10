c9dc08aa7f5ac18e32b2de544dfd7bf22b9deae2
oldgeezr <ramstadevensen@hotmail.com>
2014-03-10 16:57:43 +0100

    new do

--- network/network.go
+++ network/network.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	UDP_PORT  string = ":39771"
+	UDP_PORT  string = ":39777"
 	TCP_PORT  string = ":27731"
 	BROADCAST string = "129.241.187.255"
 	IP_BASE   string = "129.241.187."
