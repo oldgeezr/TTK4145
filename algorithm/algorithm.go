9b6b2fb606f4ad6487942ee234825733c3fb5ecd
oldgeezr <ramstadevensen@hotmail.com>
2014-03-10 17:25:01 +0100

    new do

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -20,9 +20,10 @@ func Algo(get_at_floor chan Dict, get_queues chan Queues) {
 			ext_queue := queues.Ext_queue
 			last_queue := queues.Last_queue
 
-			for _, last := range last_queue {
+			for i, last := range last_queue {
 				if last.Ip_order == at_floor.Ip_order {
 					last_dir = last.Dir
+					current_last = i
 				}
 			}
 
@@ -38,6 +39,8 @@ func Algo(get_at_floor chan Dict, get_queues chan Queues) {
 							Println("QUEUE:", int_queue)
 							int_queue[i].Dest = Insert_at_pos("ip_order", int_queue[i].Dest, at_floor.Floor, 0)
 							Println("QUEUE2:", int_queue)
+							go func() { get_at_floor <- at_floor }()
+							break
 						}
 					}
 				}
