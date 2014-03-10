c5624cc2a0eab0a772ec1a2be55aa045e9aa3972
oldgeezr <ramstadevensen@hotmail.com>
2014-03-10 15:35:40 +0100

    new do

--- lift/log/log.go
+++ lift/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	. "../.././functions"
+	. "../.././network"
 	. "fmt"
 )
 
@@ -33,7 +34,7 @@ func Job_queues(master_order, slave_order, get_at_floor chan Dict, queues, get_q
 				ext_queue, _ = AIM_Spice(ext_queue, msg.Floor, msg.Dir)
 				the_queue = Queues{job_queue, ext_queue, last_queue}
 			} else if msg.Floor >= M {
-				last_queue, update = AIM_Dict2(last_queue, msg)
+				last_queue, _ = AIM_Dict2(last_queue, msg)
 			} else if msg.Dir == "standby" {
 				var update bool
 				last_queue, update = AIM_Dict(last_queue, msg)
