2c6b7ec697e5147c49261b402c4109b4b0786f0d
oldgeezr <ramstadevensen@hotmail.com>
2014-03-10 18:45:34 +0100

    new do

--- lift/internal.go
+++ lift/internal.go
@@ -52,6 +52,7 @@ func Do_first(do_first chan Queues, order chan Dict, kill_send_to_floor chan boo
 									kill_send_to_floor <- true
 								}
 							}()
+							running = true
 							go func() {
 								running = Send_to_floor(yours.Dest[0].Floor, last_floor, "int", kill_send_to_floor)
 							}()
@@ -66,6 +67,7 @@ func Do_first(do_first chan Queues, order chan Dict, kill_send_to_floor chan boo
 										kill_send_to_floor <- true
 									}
 								}()
+								running = true
 								go func() {
 									running = Send_to_floor(yours.Dest[0].Floor, last_floor, "int", kill_send_to_floor)
 								}()
