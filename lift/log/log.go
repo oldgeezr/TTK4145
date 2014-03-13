bbd1075124b1969efea9b6432d67ddcd3bef7e46
oldgeezr <ramstadevensen@hotmail.com>
2014-03-13 14:49:16 +0100

    ok

--- lift/log/log.go
+++ lift/log/log.go
@@ -103,12 +103,17 @@ func Job_queues(master_order, slave_order, get_at_floor chan Dict, queues, get_q
 			}
 			the_queue = Queues{job_queue, ext_queue, last_queue}
 			slave_queues <- the_queue
-		case the_queue = <-get_queues:
+		case msg := <-get_queues:
+			the_queue.Int_queue = msg.Int_queue
+			the_queue.Ext_queue = msg.Ext_queue
+			the_queue.Last_queue = msg.Last_queue
 			Println("TO LOG:")
 			Format_queues_term(the_queue)
 			slave_queues <- the_queue
 		case msg := <-queues:
-			the_queue = msg
+			the_queue.Int_queue = msg.Int_queue
+			the_queue.Ext_queue = msg.Ext_queue
+			the_queue.Last_queue = msg.Last_queue
 		case do_first <- the_queue: // DO FIRST
 		case get_queues <- the_queue: // ALGO
 			Println("TO ALGO:")
