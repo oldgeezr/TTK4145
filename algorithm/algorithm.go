4b0790cf5acec26a9f6974213c03ad54e3b8024e
oldgeezr <ramstadevensen@hotmail.com>
2014-03-13 13:13:40 +0100

    ok

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -11,7 +11,7 @@ func Algo(get_at_floor chan Dict, get_queues chan Queues) {
 	Fo.WriteString("Entered Algo\n")
 
 	var last_dir string
-	// var current_queue Jobs
+	var current_queue Jobs
 
 	for {
 		at_floor := <-get_at_floor
@@ -31,7 +31,7 @@ func Algo(get_at_floor chan Dict, get_queues chan Queues) {
 
 		for i, yours := range int_queue { // Gå gjennom alle jobbkøene
 			if yours.Ip == at_floor.Ip_order { // Finn riktig jobbkø
-				// current_queue = yours
+				current_queue = yours
 				current_index = i
 			}
 		}
@@ -39,32 +39,24 @@ func Algo(get_at_floor chan Dict, get_queues chan Queues) {
 		Println("ALGO:")
 		Format_queues_term(queues)
 
-		Println("ALGO:", int_queue)
-		Println("ALGO:", current_index)
-		Println("ALGO:", int_queue[current_index])
-		Println("ALGO:", int_queue[current_index].Dest, at_floor.Floor)
-		Println("ALGO:", int_queue[current_index].Dest[0].Floor, at_floor.Floor)
-
 		if !Missing_ext_job(ext_queue, at_floor.Floor, last_dir) { // Noen skal på
-			if int_queue[current_index].Dest[0].Floor != at_floor.Floor {
+			if len(int_queue[current_index].Dest) != 0 {
+				if int_queue[current_index].Dest[0].Floor != at_floor.Floor {
+					int_queue[current_index].Dest = Insert_at_pos("ip_order", int_queue[current_index].Dest, at_floor.Floor, 0)
+				}
+			} else {
 				int_queue[current_index].Dest = Insert_at_pos("ip_order", int_queue[current_index].Dest, at_floor.Floor, 0)
 			}
-			/*if len(ext_queue) != 0 {
+			if len(ext_queue) != 0 {
 				ext_queue = Remove_order_ext_queue(ext_queue, at_floor.Floor, last_dir)
-			}*/
+			}
 		}
 
-		Println("ALGO:", ext_queue, at_floor.Floor, last_dir)
-		Println("ALGO:", int_queue[current_index].Dest[0].Floor, at_floor.Floor)
-		Println("ALGO:", int_queue[current_index].Dest, at_floor.Floor)
-
-		/*if !Missing_int_job(current_queue, at_floor.Floor) { // Noen skal av
-			if current_queue.Dest[0].Floor == at_floor.Floor {
-				// Skal fjerne etg fra internjobbkø
-				if len(int_queue[current_index].Dest) != 0 {
+		if !Missing_int_job(current_queue, at_floor.Floor) { // Noen skal av
+			if len(current_queue.Dest) != 0 {
+				if current_queue.Dest[0].Floor == at_floor.Floor {
+					// Skal fjerne etg fra internjobbkø
 					int_queue[current_index] = Remove_order_int_queue(int_queue[current_index], at_floor.Floor)
-				}
-				if len(ext_queue) != 0 {
 					ext_queue = Remove_order_ext_queue(ext_queue, at_floor.Floor, last_dir)
 				}
 			} else {
@@ -72,7 +64,7 @@ func Algo(get_at_floor chan Dict, get_queues chan Queues) {
 				int_queue[current_index] = Remove_order_int_queue(int_queue[current_index], at_floor.Floor)
 				int_queue[current_index].Dest = Insert_at_pos("ip_order", int_queue[current_index].Dest, at_floor.Floor, 0)
 			}
-		}*/
+		}
 
 		queues = Queues{int_queue, ext_queue, last_queue}
 		get_queues <- queues
