1d2954611350928ae9bbe038d1f44f63c59a9aaf
talosmith <erlendh@me.com>
2014-03-17 20:25:51 +0100

    erlend update

--- lift/log/log.go
+++ lift/log/log.go
@@ -62,7 +62,7 @@ func Job_queues(log_order, get_at_floor chan Dict, queues, get_queues, set_queue
 		case do_first <- the_queue: // DO FIRST
 		case get_queues <- the_queue: // ALGO
 			Format_queues_term(the_queue, "LOG")
-			the_queue = Queues{}
+			//the_queue = Queues{}
 		}
 	}
 }
