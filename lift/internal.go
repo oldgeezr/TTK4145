4fb11f7296f56b8dea16a77d101361cbb2b09cbc
talosmith <erlendh@me.com>
2014-03-17 20:35:12 +0100

    erlend update

--- lift/internal.go
+++ lift/internal.go
@@ -26,7 +26,7 @@ func Do_first(do_first chan Queues, order chan Dict) {
 
 		job_queue := queues.Int_queue
 		ext_queue := queues.Ext_queue
-		//last_queue := queues.Last_queue
+		last_queue := queues.Last_queue
 
 		if Get_floor_sensor() != -1 {
 			last_floor = Get_floor_sensor()
