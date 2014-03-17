48096aefd3260876d0f0bf158b368022494ac7ad
talosmith <erlendh@me.com>
2014-03-17 20:23:30 +0100

    erlend update

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -52,6 +52,8 @@ func Algo(get_at_floor chan Dict, get_queues, set_queues chan Queues) {
 			Println("Stage: 6")
 			ext_queue = Remove_order_ext_queue(ext_queue, at_floor.Floor, last_dir)
 		}
+
+		queues = Queues{int_queue, ext_queue, last_queue}
 		Format_queues_term(queues, "ALGO")
 
 		if !Missing_int_job(current_queue, at_floor.Floor) { // Noen skal av
