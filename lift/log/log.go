59bc393e84c2c22d35ca476550dc20a83920cb30
oldgeezr <ramstadevensen@hotmail.com>
2014-03-17 13:38:37 +0100

    coffee

--- lift/log/log.go
+++ lift/log/log.go
@@ -18,50 +18,19 @@ func Job_queues(master_order, slave_order, get_at_floor chan Dict, queues, get_q
 
 	for {
 		select {
-		case msg := <-master_order: // MASTER
-			if msg.Dir == "int" { // Fikk internordre
-				job_queue = ARQ(job_queue, msg)
-			} else if msg.Ip_order == "ext" {
-				ext_queue, _ = AIM_Spice(ext_queue, msg.Floor, msg.Dir)
-			} else if msg.Floor >= M {
-				var update bool
-				last_queue, update = AIM_Dict2(last_queue, msg)
-				if update {
-					get_at_floor <- msg
-				}
-			} else if msg.Dir == "standby" {
-				if len(last_queue) != 0 {
-					for _, last := range last_queue {
-						if last.Ip_order != msg.Ip_order {
-							job_queue, _ = AIM_Jobs(job_queue, msg.Ip_order)
-						}
-					}
-				} else {
-					job_queue, _ = AIM_Jobs(job_queue, msg.Ip_order)
-				}
-				var update bool
-				last_queue, update = AIM_Dict(last_queue, msg)
-				if update {
-					get_at_floor <- msg
-					Println("Lastfloor update")
-				}
-			} else if msg.Dir == "remove" {
-				get_at_floor <- msg
-				Println("Removing")
-			}
-			the_queue = Queues{job_queue, ext_queue, last_queue}
 		case msg := <-slave_order:
-			if msg.Dir == "int" {
+			switch {
+			case msg.Dir == "int":
 				job_queue = ARQ(job_queue, msg)
-			} else if msg.Ip_order == "ext" {
+			case msg.Ip_order == "ext":
 				ext_queue, _ = AIM_Spice(ext_queue, msg.Floor, msg.Dir)
-			} else if msg.Floor >= M {
+			case msg.Floor >= M:
 				var update bool
 				last_queue, update = AIM_Dict2(last_queue, msg)
 				if update {
 					get_at_floor <- msg
 				}
-			} else if msg.Dir == "standby" {
+			case msg.Dir == "standby":
 				if len(last_queue) != 0 {
 					for _, last := range last_queue {
 						if last.Ip_order != msg.Ip_order {
@@ -77,23 +46,17 @@ func Job_queues(master_order, slave_order, get_at_floor chan Dict, queues, get_q
 					get_at_floor <- msg
 					Println("Lastfloor update")
 				}
-			} else if msg.Dir == "remove" {
+			case msg.Dir == "remove":
 				get_at_floor <- msg
 				Println("Removing")
 			}
 			the_queue = Queues{job_queue, ext_queue, last_queue}
 			slave_queues <- the_queue
 		case msg := <-set_queues:
-			// Println("TO LOG:")
-			// Format_queues_term(msg)
-			the_queue.Int_queue = msg.Int_queue
-			the_queue.Ext_queue = msg.Ext_queue
-			the_queue.Last_queue = msg.Last_queue
+			the_queue = msg
 			slave_queues <- the_queue
 		case msg := <-queues:
-			the_queue.Int_queue = msg.Int_queue
-			the_queue.Ext_queue = msg.Ext_queue
-			the_queue.Last_queue = msg.Last_queue
+			the_queue = msg
 		case do_first <- the_queue: // DO FIRST
 		case get_queues <- the_queue: // ALGO
 			the_queue = Queues{}
