6cfbbd3e64b157ede20fadf13858e31925e0511f
talosmith <erlendh@me.com>
2014-03-17 20:29:03 +0100

    erlend update

--- algorithm/algorithm.go
+++ algorithm/algorithm.go
@@ -53,9 +53,6 @@ func Algo(get_at_floor chan Dict, get_queues, set_queues chan Queues) {
 			ext_queue = Remove_order_ext_queue(ext_queue, at_floor.Floor, last_dir)
 		}
 
-		queues = Queues{int_queue, ext_queue, last_queue}
-		Format_queues_term(queues, "ALGO")
-
 		if !Missing_int_job(current_queue, at_floor.Floor) { // Noen skal av
 			if len(current_queue.Dest) != 0 {
 				if current_queue.Dest[0].Floor == at_floor.Floor {
@@ -70,6 +67,7 @@ func Algo(get_at_floor chan Dict, get_queues, set_queues chan Queues) {
 		}
 
 		queues = Queues{int_queue, ext_queue, last_queue}
+		Format_queues_term(queues, "ALGO")
 		set_queues <- queues
 	}
 }
