d6f7f22b56314b2640540aa7f1c3c373c18e5276
sjokolAda <freworr@gmail.com>
2014-03-19 20:34:27 +0100

    algoupdate

--- lift/lift.go
+++ lift/lift.go
@@ -21,6 +21,7 @@ func Do_first(do_first chan Queues, order chan Dict) {
 
 	var last_floor int
 	var myIP string = GetMyIP()
+	var executing bool = false
 
 	state := make(chan string)
 
@@ -50,13 +51,17 @@ func Do_first(do_first chan Queues, order chan Dict) {
 					} else {
 						if len(ext_queue) != 0 {
 
-							if Determine_best_elevator(ext_queue, last_queue, myIP) {
+							if !executing {
+								executing = Determine_best_elevator(ext_queue, last_queue, myIP)
+							}
+							if executing {
 								if ext_queue[0].Floor > last_floor {
 									state <- "up"
 								} else if ext_queue[0].Floor < last_floor {
 									state <- "down"
 								} else {
 									state <- "stop"
+									executing = false
 									Fprintf(Fo, "Ext:stop\n")
 								}
 							} else {
@@ -67,6 +72,7 @@ func Do_first(do_first chan Queues, order chan Dict) {
 							state <- "standby"
 							Fprintf(Fo, "Ext:standby\n")
 						}
+
 					}
 				}
 			}
