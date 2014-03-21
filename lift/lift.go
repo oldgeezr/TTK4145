40b32686e507971b2909c59930cfda126438b72a
oldgeezr <ramstadevensen@hotmail.com>
2014-03-21 20:20:00 +0100

    clone

--- lift/lift.go
+++ lift/lift.go
@@ -1,21 +1,20 @@
 package lift
 
 import (
-/*. ".././driver"
-// . ".././formating"
-. ".././functions"
-. ".././network"
-// . "./log"
-. "fmt"
-. "strconv"
-"time"*/
+	. ".././driver"
+	// . ".././formating"
+	. ".././functions"
+	. ".././network"
+	// . "./log"
+	. "fmt"
+	. "strconv"
+	"time"
 )
 
 const (
 	M int = 4 // Number of floors
 )
 
-/*
 func Do_first(do_first chan Queues, order chan Dict) {
 
 	Fo.WriteString("Entered Do_first\n")
@@ -231,4 +230,3 @@ func Lift_init(do_first chan Queues, order chan Dict) {
 	go External_lights(do_first)
 	go Do_first(do_first, order)
 }
-*/
