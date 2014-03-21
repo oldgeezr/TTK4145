006871177471e5ddca8543b35478fb132cee758c
talosmith <erlendh@me.com>
2014-03-21 14:37:48 +0100

    updates

--- functions/functions.go
+++ functions/functions.go
@@ -1,7 +1,6 @@
 package functions
 
 import (
-	// . ".././lift"
 	. "fmt"
 	"os"
 	"time"
@@ -50,6 +49,7 @@ func Insert_at_pos(ip string, this []Dict, value, pos int) []Dict {
 	return this
 }
 
+/*
 func Pop_first(this []Dict) []Dict {
 
 	return this[1:len(this)]
@@ -60,6 +60,7 @@ func Read_first(this []Dict) int {
 	return this[len(this)-1].Floor
 }
 
+
 func Remove_from_pos(this []Dict, floor int) []Dict {
 
 	for i, order := range this {
@@ -69,6 +70,7 @@ func Remove_from_pos(this []Dict, floor int) []Dict {
 	}
 	return this
 }
+*/
 
 func Append_if_missing_queue(queues []Jobs, ip string) ([]Jobs, bool) {
 
@@ -108,6 +110,7 @@ func Append_if_missing_dict(slice []Dict, last Dict) ([]Dict, bool) {
 	return append(slice, last), false
 }
 
+/*
 func Update_Direction(slice []Dict, last Dict) ([]Dict, bool) {
 
 	for i, yours := range slice {
@@ -121,7 +124,7 @@ func Update_Direction(slice []Dict, last Dict) ([]Dict, bool) {
 	}
 	return append(slice, last), true
 }
-
+*/
 func Append_if_missing_ext_queue(slice []Dict, floor int, dir string) ([]Dict, bool) {
 	Println("AIM_ext: ", slice, floor, dir)
 	for _, yours := range slice {
@@ -142,31 +145,13 @@ func Append_if_missing_ip(slice []int, i int) []int {
 	return append(slice, i)
 }
 
-func Someone_getting_off(job_queue Jobs, floor int) bool {
-
-	if len(job_queue.Dest) != 0 {
-		for _, orders := range job_queue.Dest {
-			if orders.Floor == floor {
-				return true
-			}
+func Append_if_missing_right_queue(queue []Jobs, msg Dict) []Jobs {
+	for i, job := range queue {
+		if job.Ip == msg.Ip_order {
+			queue[i].Dest, _ = Append_if_missing_floor(queue[i].Dest, msg.Floor)
 		}
 	}
-	return false
-}
-
-func Someone_getting_on(job_queue []Dict, floor int, dir string) bool {
-	//Print("Someone_on: ", floor, dir)
-	if len(job_queue) != 0 {
-		for _, orders := range job_queue {
-			if orders.Floor == floor && (dir == orders.Dir || dir == "standby") {
-				Println("true")
-				return true
-			}
-		}
-
-	}
-	Println("false")
-	return false
+	return queue
 }
 
 func Remove_dict_ext_queue(this []Dict, floor int, dir string) []Dict {
@@ -202,6 +187,34 @@ func Remove_job_queue(this Jobs, floor int) Jobs {
 
 }
 
+func Someone_getting_off(job_queue Jobs, floor int) bool {
+
+	if len(job_queue.Dest) != 0 {
+		for _, orders := range job_queue.Dest {
+			if orders.Floor == floor {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func Someone_getting_on(job_queue []Dict, floor int, dir string) bool {
+	//Print("Someone_on: ", floor, dir)
+	if len(job_queue) != 0 {
+		for _, orders := range job_queue {
+			if orders.Floor == floor && (dir == orders.Dir || dir == "standby") {
+				Println("true")
+				return true
+			}
+		}
+
+	}
+	Println("false")
+	return false
+}
+
+/*
 func Determine_best_elevator(Ext_queue []Dict, Last_queue []Dict, myIP string) bool {
 
 	var best int = 100
@@ -224,16 +237,9 @@ func Determine_best_elevator(Ext_queue []Dict, Last_queue []Dict, myIP string) b
 	}
 
 }
+*/
 
-func Append_if_missing_right_queue(queue []Jobs, msg Dict) []Jobs {
-	for i, job := range queue {
-		if job.Ip == msg.Ip_order {
-			queue[i].Dest, _ = Append_if_missing_floor(queue[i].Dest, msg.Floor)
-		}
-	}
-	return queue
-}
-
+/*
 func Determine_dir(job_queue []Jobs, last Dict) string {
 	for _, job := range job_queue {
 		if last.Ip_order == job.Ip {
@@ -252,3 +258,4 @@ func Determine_dir(job_queue []Jobs, last Dict) string {
 	}
 	return "standby"
 }
+*/
