fb81efb5bf40f531957e87743d8f76f3cf682b40
FWorren <freworr@gmail.com>
2014-03-22 01:23:45 +0100

    semnd

--- main.go
+++ main.go
@@ -19,23 +19,13 @@ func main() {
 	state := make(chan bool)
 	b := make([]byte, 1024)
 	lost_conn := make(chan bool)
+	error_chan := make(chan error)
 
 	Println("LISTENING FOR NETWORK ACTIVITY")
 
-	// Initiate program
-	saddr, _ := ResolveUDPAddr("udp", "localhost"+UDP_PORT_net+GetMyIP())
-	ln, _ := ListenUDP("udp", saddr)
-	ln.SetReadDeadline(time.Now().Add(200 * time.Millisecond))
-	_, _, err := ln.ReadFromUDP(b)
-	ln.Close()
-	// Initiate program -- END
-
-	Println("NETWORK ERROR:", err)
-
-	Println("DONE LISTENING")
-
 	go func() {
-		if err != nil {
+		st := <-error_chan
+		if st != nil {
 			Println("EVALUATE ERROR != nil")
 			state <- true
 			Println("BECOME THE MASTER")
@@ -45,11 +35,25 @@ func main() {
 		}
 	}()
 
+	// Initiate program
+	saddr, _ := ResolveUDPAddr("udp", "localhost"+UDP_PORT_net+GetMyIP())
+	ln, _ := ListenUDP("udp", saddr)
+	ln.SetReadDeadline(time.Now().Add(200 * time.Millisecond))
+	_, _, err := ln.ReadFromUDP(b)
+	error_chan <- err
+	ln.Close()
+	// Initiate program -- END
+
+	Println("NETWORK ERROR:", err)
+
+	Println("DONE LISTENING")
+
 	for {
 		Println("LISTENING FOR STATE")
 		master = <-state
 		Println("GOT STATE:", master)
 		Println("DONE LISTENING FOR STATE")
+		time.Sleep(100 * time.Millisecond)
 		switch {
 		case master:
 			// --------------------------------- Start: Searching for net connection ------------------------------------
